883fa8408091884173d4506647fdd512032a1214
ZicongWu <786019674@qq.com>
2021-04-06 14:47:02 +0800

    use structure

--- main.c
+++ main.c
@@ -2,110 +2,121 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-//#include <mpi.h>
 
-char* Grid_Init(int n);
-void Grid_Disp(char *grid, unsigned int npt, unsigned int not);
-void Move_Red(char *grid, unsigned int raw);
-void Move_Blue(char *grid, unsigned int raw);
-char Is_sotp(char *grid,float c ,unsigned int npt, unsigned int not, unsigned int gsz,unsigned int raw, unsigned int bot);
+
+typedef struct
+{
+    unsigned int npt;   //number of rows per tile
+    unsigned int not;   //number of tiles
+    unsigned int c;     //terminate condition in percent
+    unsigned int ths;   //terminal threshold in number
+    unsigned int row;   //number of rows 
+    unsigned int cpt;   //number of cells per tile
+    unsigned int bsz;   //board size (number of cells in the board}
+    char *grid;         //pointer to the satrt address of the board
+}Board_Type;
+
+
+void Grid_Init(Board_Type *board);
+void Grid_Disp(Board_Type *board);
+void Move_Red(Board_Type *board);
+void Move_Blue(Board_Type *board);
+char Is_sotp(Board_Type *board);
 
 
 int main(int argc , char **argv)
 {
-    char *grid;
+    Board_Type board;
+    
+    // char *grid;
+    // unsigned int npt;
+    // unsigned int not;
+    // unsigned int c;
+    // unsigned int row;
+    // unsigned int cpt;
+    // unsigned int bsz;
+    // unsigned int ths;
 
-    unsigned int npt;
-    unsigned int not;
-    float c=5;
-    unsigned int raw;
-    unsigned int bot;
-    unsigned int gsz;
-    unsigned int ths;
-
-    printf("number of raws per taill:");
-    scanf("%d",&npt);
+    printf("number of rows per taill:");
+    scanf("%d",&board.npt);
     printf("number of taills:");
-    scanf("%d",&not);
-    printf("threshold(c):");
-    scanf("%f",&c);
-    printf("\r\n npt=%d,not=%d,c=%f\r\n",npt,not,c);
+    scanf("%d",&board.not);
+    printf("threshold(c%%):");
+    scanf("%d%%",&board.c);
+    printf("\r\n npt=%d,not=%d,c=%d%%\r\n",board.npt,board.not,board.c);
 
-    raw = npt * not;
-    bot = npt * npt;
-    gsz = (unsigned int)pow(raw,2);
-    ths = npt * npt * c;
+    board.row = board.npt * board.not;
+    board.cpt = board.npt * board.npt;
+    board.bsz = (unsigned int)pow(board.row,2);
+    board.ths = board.npt * board.npt * board.c * 0.01;
 
-    grid = Grid_Init(gsz);
-    Grid_Disp(grid,npt,not);
-    while (!Is_sotp(grid, c,npt,not,gsz,raw,bot))
+    Grid_Init(&board);
+    Grid_Disp(&board);
+    while (!Is_sotp(&board))
     {
-         Move_Red(grid,raw);
-         Move_Blue(grid,raw);
-         Grid_Disp(grid,npt,not);
+         Move_Red(&board);
+         Move_Blue(&board);
+         Grid_Disp(&board);
     }
-    Grid_Disp(grid,npt,not);
+    Grid_Disp(&board);
     return 0;
 }
 
-char* Grid_Init(int n)
+
+void Grid_Init(Board_Type *board)
 {
-    char *grid;
     unsigned int i;
     unsigned int r;
-    grid = (char *)malloc(n);
+    board->grid = (char *)malloc(board->bsz);
     srand(time(0));
-    for (i = 0; i != n; i++)
+    for (i = 0; i != board->bsz; i++)
     {
         r = rand()%100;
         if (r < 33)
-            *(grid + i) = 'R';
+            *(board->grid + i) = 'R';
         else if (r < 66)
-            *(grid + i) = 'B';
+            *(board->grid + i) = 'B';
         else
-            *(grid + i) = 'W';
-        }
-    return grid;
+            *(board->grid + i) = 'W';
+    }
 }
 
-void Grid_Disp(char *grid, unsigned int npt, unsigned int not)
-{
+void Grid_Disp(Board_Type *board)
+{  
     unsigned int i, j;
-    unsigned int raw = npt*not;
-    char sep;
     printf("\r\n");
-    for (i=0;i!=raw*4+1;i++)
+    for (i=0;i!=board->row*4+1;i++)
         printf("=");
     printf("\r\n");
-    for (i=0;i!=raw;i++)
+    for (i=0;i!=board->row;i++)
     {
         printf("|");
-        for (j=0;j!=raw;j++)
+        for (j=0;j!=board->row;j++)
         {
-            if ((j+1)%not == 0)
+            if ((j+1)%board->not == 0)
             {
-                if (*(grid+i*raw+j) != 'W')//only print R&B
-                    printf(" %c |",*(grid+i*raw+j));
+                if (*(board->grid+i*board->row+j) != 'W')//only print R&B
+                    printf(" %c |",*(board->grid+i*board->row+j));
                 else
                     printf("   |");                
             }
             else
             {
-                if (*(grid+i*raw+j) != 'W')//only print R&B
-                    printf(" %c  ",*(grid+i*raw+j));
+                if (*(board->grid+i*board->row+j) != 'W')//only print R&B
+                    printf(" %c  ", *(board->grid+i*board->row+j) );
                 else
                     printf("    ");
             }
         }
         printf("\r\n");  
-        if((i+1)%not == 0)
+        if((i+1)%board->not == 0)
         {
-            for (j=0;j!=raw*4+1;j++)
+            for (j=0;j!=board->row*4+1;j++)
                 printf("=");
         }
         else
         {
-            for (j=0;j!=raw*4+1;j++)
+            for (j=0;j!=board->row*4+1;j++)
                 printf("-");
         }    
         printf("\r\n");            
@@ -114,16 +125,16 @@ void Grid_Disp(char *grid, unsigned int npt, unsigned int not)
 }
 
 
-void Move_Red(char *grid, unsigned int raw)
+void Move_Red(Board_Type *board)
 {
     unsigned int i, j;
     char *cur,*dist;
-    for(i = 0; i!=raw ;i++)
+    for(i = 0; i!=board->row ;i++)
     {
-        for(j=0; j<raw ; j++)
+        for(j=0; j<board->row ; j++)
         {
-            cur = grid+i*raw+j;
-            dist = grid+i*raw+((j+1)%raw);
+            cur = board->grid+i*board->row+j;
+            dist = board->grid+i*board->row+((j+1)%board->row);
             if (*cur == 'R' && *dist == 'W')
             {
                 *dist='R';
@@ -134,16 +145,16 @@ void Move_Red(char *grid, unsigned int raw)
     } 
 }
 
-void Move_Blue(char *grid, unsigned int raw)
+void Move_Blue(Board_Type *board)
 {
     unsigned int i, j;
     char *cur,*dist;
-    for(j = 0; j!=raw ;j++)
+    for(j = 0; j!=board->row ;j++)
     {
-        for(i=0; i<raw ; i++)
+        for(i=0; i<board->row ; i++)
         {
-            cur = grid+i*raw+j;
-            dist = grid+((i+1)%raw)*raw+j;
+            cur = board->grid+i*board->row+j;
+            dist = board->grid+((i+1)%board->row)*board->row+j;
             if (*cur == 'B' && *dist == 'W')
             {
                 *dist='B';
@@ -153,28 +164,28 @@ void Move_Blue(char *grid, unsigned int raw)
         }
     } 
 }
-char Is_sotp(char *grid,float c ,unsigned int npt, unsigned int not, unsigned int gsz,unsigned int raw, unsigned int bot)
+char Is_sotp(Board_Type *board)
 {
     unsigned int i,j,p,q;
     unsigned int rint,bint;
-    for (i=0;i!=not;i++)
+    for (i=0;i!=board->not;i++)
     {
-        for(j=0;j!=not;j++)
+        for(j=0;j!=board->not;j++)
         {
             rint=0;
             bint=0;
-            for(p=i*npt;p!=(i+1)*npt;p++)
+            for(p=i*board->npt;p!=(i+1)*board->npt;p++)
             {
-                for(q=j*npt;q!=(j+1)*npt;q++)
+                for(q=j*board->npt;q!=(j+1)*board->npt;q++)
                 {
-                    if(*(grid + p*raw + q )=='R')
+                    if(*(board->grid + p*board->row + q )=='R')
                         rint++;
-                    else if(*(grid + p*raw + q )=='B')
+                    else if(*(board->grid + p*board->row + q )=='B')
                         bint++;
                 }
             }
-            if(rint>=c*bint || bint>=c*rint)
-                return 1;       
+//            if(rint>=c*bint || bint>=c*rint)
+//                return 1;       
         }
     }
     return 0;
